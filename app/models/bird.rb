deb02a92edcbab0d1dfb70eed5ed9a8a6ad6aa92
NaurisM <65161296+NaurisM@users.noreply.github.com>
2021-12-01 17:30:30 +0200

    some validation and birds

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,3 +1,4 @@
 class Bird < ApplicationRecord
   has_one :observation
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,4 +1,6 @@
 class Observation < ApplicationRecord
   belongs_to :user
   has_one :bird
+  validates :name, uniqueness: { scope: :season, message: "should be added once per season" }
+  validates :max_number, numericality: { greater_than: 0 }
 end
